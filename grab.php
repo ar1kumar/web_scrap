bf76fe4d7c53948a0314fe2471b1f9da712ea496
Ar1k <arunkmr492@gmail.com>
2015-04-07 17:39:27 +0530

    issues resolved

--- grab.php
+++ grab.php
@@ -10,7 +10,7 @@
 					  echo "<img height=\"200\" src='{$result['responseData']['results'][$i]['tbUrl']}' />";
 					  echo "<br/>";
 					  $content = $result['responseData']['results'][$i]['tbUrl'];
-					  file_put_contents('grabs/image'.[$i].'.jpg', $content);
+					  file_put_contents('grabs/image'.$i.'.jpg', $content);
 				  }
 				 
 				 // Create DOM from URL or file
